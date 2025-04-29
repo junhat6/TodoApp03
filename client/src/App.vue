b3efe7765c96fd97a5af9ea5e1333d0fd64edb6e
junichi.hattori <junhat6@gmail.com>
2025-04-29 15:36:21 +0900

    TodoListを追加。最小限のUI完成。

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,21 @@
 <script setup lang="ts">
 import TodoInput from './components/TodoInput.vue';
+import TodoList from './components/TodoList.vue';
+import { useTodoStore } from './store/todo';
+
+const store = useTodoStore()
 </script>
 
 <template>
   <h1>My Todo App</h1>
   <TodoInput />
+  <TodoList />
+  <p>残り： {{ store.remaining }}</p>
 </template>
 
-<style scoped>
-
+<style>
+body {
+  font-family: sans-serif;
+  padding: 2rem;
+}
 </style>
---
+++ client/src/components/TodoList.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { useTodoStore } from '../store/todo';
+const store = useTodoStore()
+</script>
+
+
+<template>
+<ul class="todo-list">
+    <li v-for="todo in store.todos" :key="todo.id" :class="{completed: todo.completed}">
+        <label>
+            <input
+            type="checkbox"
+            :checked="todo.completed"
+            @change="store.toggleTodo(todo.id)"
+            />
+            {{ todo.text }}
+        </label>
+        <button class="remove" @click="store.removeTodo(todo.id)">✗</button>
+    </li>
+</ul>
+</template>
+
+<style scoped>
+.todo-list {
+  list-style: none;
+  padding: 0;
+}
+.todo-list li {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0.5rem;
+  border-bottom: 1px solid #ddd;
+}
+.todo-list li.completed label {
+  text-decoration: line-through;
+  color: #999;
+}
+.todo-list button.remove {
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  font-size: 1.2rem;
+  line-height: 1;
+}
+</style>
