1d136ad49ea2b78175764f05898ab1d043a3f206
junichi.hattori <junhat6@gmail.com>
2025-04-29 14:22:53 +0900

    TodoInputを作成。

---
+++ client/src/components/TodoInput.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useTodoStore } from '../store/todo';
+
+const store = useTodoStore()
+const text = ref('')
+
+function onAdd() {
+    const v = text.value &&text.value.trim()
+    if(!v) return
+
+    store.addTodo(v)
+    text.value = ''
+
+}
+</script>
+
+<template>
+    <div class="todo-input">
+        <input 
+        v-model="text"
+        @keyup.enter="onAdd"
+        placeholder="新しいタスクを入力"
+        />
+        <button @click="onAdd">追加</button>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
