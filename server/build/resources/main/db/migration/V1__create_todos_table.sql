9bf09cd5231cb857e9505d6f0da5256f7968eaab
junichi.hattori <junhat6@gmail.com>
2025-10-30 13:36:03 +0900

    gitignore追加

--- server/build/resources/main/db/migration/V1__create_todos_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE
-    todos (
-        id UUID PRIMARY KEY,
-        text VARCHAR(100) NOT NULL,
-        category VARCHAR(50) NOT NULL,
-        completed BOOLEAN NOT NULL DEFAULT FALSE,
-        created_at TIMESTAMP NOT NULL,
-        updated_at TIMESTAMP NOT NULL
-    );
